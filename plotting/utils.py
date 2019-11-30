bc5c7f280b82aa26f163816bb41788270044d46e
Brendan Blanchard <brendano257@gmail.com>
2019-11-30 13:30:13 -0500

    Moved default of datetime.now() into create_daily_ticks

--- plotting/utils.py
+++ plotting/utils.py
@@ -6,7 +6,7 @@ from dateutil.relativedelta import relativedelta
 __all__ = ['create_daily_ticks', 'create_monthly_ticks']
 
 
-def create_daily_ticks(days_in_plot, minors_per_day=4, end_date=datetime.now()):
+def create_daily_ticks(days_in_plot, minors_per_day=4, end_date=None):
     """
     Takes a number of days to plot back, and creates major (1 day) and minor (6 hour) ticks by default.
 
@@ -15,6 +15,9 @@ def create_daily_ticks(days_in_plot, minors_per_day=4, end_date=datetime.now()):
     :param datetime end_date: the final day that will be on the plot, defaults to today
     :return tuple: date_limits, major_ticks, minor_ticks
     """
+
+    if not end_date:
+        end_date = datetime.now()
 
     date_limits = dict()
     date_limits['right'] = end_date.replace(hour=0, minute=0, second=0, microsecond=0) + dt.timedelta(
