b6998022d53462e8b597a54ff1f3329200ac88b0
Brendan Blanchard <brendano257@gmail.com>
2019-12-08 14:59:50 -0500

    Added docs and cleaned up

--- lab/compare_entity_to_db/check_against_database.py
+++ lab/compare_entity_to_db/check_against_database.py
@@ -16,13 +16,19 @@ from IO.db import connect_to_db, Base, LogFile, Integration, Daily, DailyFile
 
 def filter_for_new_entities(objs, orm_class, attr, session=None):
     """
-    Filter a list of sqlalchemy class instances for only those that are unique along the provided attribute.
+    Filter a list of sqlalchemy class instances for only those whose attribute attr are not already in the databse.
 
-    :param objs:
-    :param orm_class:
-    :param attr:
-    :param session:
-    :return:
+    The provided list of objects is checked against the database by returning only objects whose specified attribute
+    does not match any entry in the database for that class. Frequently used with paths or dates to determine what
+    unique data to add to and commit to the database.
+
+    :param Sequence objs: sequence of objects to check against the database
+    :param orm_class: Declared class of the provided objects
+    :param str attr: Attribute of the orm class to compare against the database
+        **IntegrityErrors are completely possible if attr is not actually a unique-constrained attribute of orm_class
+    :param Session session: an active sqlalchemy session to use; created and closed internally if not given
+    :return list: potentially empty list of all objs that are not already present in the database
+    :raises TypeError: if session is not a sqlalchemy session, or the orm_class is not a declared sqlalchemy class
     """
     if not type(orm_class) is type(Base):
         msg = 'orm_class must be a declared sqlalchemy class'
@@ -30,19 +36,20 @@ def filter_for_new_entities(objs, orm_class, attr, session=None):
 
     if not session:
         _, session = connect_to_db(DB_NAME, CORE_DIR)
+        close_on_exit = True
     else:
         if not type(session) is type(Session):
             msg = 'Provided session must be an active sqlalchemy session'
             raise TypeError(msg)
+        close_on_exit = False
 
     obj_attrs = [getattr(o, attr) for o in objs]
-
     obj_attr_sets = split_into_sets_of_n(obj_attrs, 750)  # avoid SQLite var limit of 1000
 
     objs_in_db = []
     for set_ in obj_attr_sets:
-        db_attrs_from_set = session.query(orm_class).filter(getattr(orm_class, attr).in_(set_)).all()
-        for item in db_attrs_from_set:
+        db_objs_from_set = session.query(orm_class).filter(getattr(orm_class, attr).in_(set_)).all()
+        for item in db_objs_from_set:
             objs_in_db.append(item)
 
     attrs_in_db = [getattr(e, attr) for e in objs_in_db]
@@ -53,6 +60,9 @@ def filter_for_new_entities(objs, orm_class, attr, session=None):
             new_objs.append(obj)  # add new object to return list
             # adding the new obj attr to the check-against list is required to prevent duplicates in one batch
             attrs_in_db.append(getattr(obj, attr))
+
+    if close_on_exit:
+        session.close()
 
     return new_objs
 
@@ -74,6 +84,7 @@ def test_on_log_files():
 
     for log in new_only:
         print(log.date)
+    print('')
 
 
 def test_on_dailies():
@@ -83,16 +94,41 @@ def test_on_dailies():
 
     print(f'DailyFiles read: {len(daily_files)}')
 
+    # use _path instead of property, sqlalchemy needs the actual field to query
     new_only = filter_for_new_entities(daily_files, DailyFile, '_path')
 
     print(f'DailyFiles returned: {len(new_only)}')
 
     for file in new_only:
         print(file.path)
+    print('')
+
+
+def test_on_integrations():
+    from processing.file_io import read_gcms_file
+    from IO import get_all_data_files
+
+    all_results = sorted(get_all_data_files(GCMS_DIR, 'integration_results.txt'))
+
+    integrations = []
+    for file in all_results:
+        integrations.append(Integration(**read_gcms_file(file)))
+
+    print(f'Integrations read: {len(integrations)}')
+
+    # use _path instead of property, sqlalchemy needs the actual field to query
+    new_only = filter_for_new_entities(integrations, Integration, 'date')
+
+    print(f'Integrations returned: {len(new_only)}')
+
+    for file in new_only:
+        print(file.path)
+    print('')
 
 
 if __name__ == '__main__':
-    # test_on_log_files()  # returns only log dates not already in the database
-    # test_on_dailies()
+    test_on_log_files()  # returns only log dates not already in the database
+    test_on_dailies()
+    test_on_integrations()  # returns nothing, but there are no new integrations as of now...
 
     pass
