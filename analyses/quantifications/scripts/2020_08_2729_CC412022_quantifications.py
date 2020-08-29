51b87b893ba7d842a3ecf4aac74fe5e8699a8fe8
Brendan Blanchard <brendano257@gmail.com>
2020-08-29 14:18:10 -0400

    Added

---
+++ analyses/quantifications/scripts/2020_08_2729_CC412022_quantifications.py
@@ -0,0 +1,16 @@
+"""
+A sequence of standards were run over three days to quantify and compare CC412022 vs SX3555.
+
+The sequence was: (CC412022, SX3555, Blank2500);
+    which was run after normal runs for three days (2020-08-27 --> 2020-08-29).
+"""
+
+from datetime import datetime
+
+from reporting import create_multiday_quant_report
+
+days_with_standards = [datetime(2020, 8, x) for x in (27, 28, 29)]
+create_multiday_quant_report('CC412022', 'SX3555', days_with_standards)
+
+days_with_standards = [datetime(2020, 8, x) for x in (27, 28, 29)]
+create_multiday_quant_report('SX3555', 'CC412022', days_with_standards)
