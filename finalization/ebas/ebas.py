84440969fd7e1c0983355235f30743acc7c7047f
Brendan Blanchard <brendano257@gmail.com>
2021-03-13 11:22:43 -0500

    Added; will build out

---
+++ finalization/ebas/ebas.py
@@ -0,0 +1,7 @@
+"""
+EBAS IO Setup Instructions: https://git.nilu.no/ebas/ebas-io/-/wikis/home
+
+For my own purposes, I've cloned the ebas-io repository here, and installed with:
+
+pip install ebas-io/dist/ebas_io-3.5.1-py3-none-any.whl
+"""
